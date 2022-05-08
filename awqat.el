231bc1ff371f1013d5e76c8b830715d3a80fbec2
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-05-08 03:36:28 +0200

    Add preset for Kuwait

--- awqat.el
+++ awqat.el
@@ -133,6 +133,10 @@ This is not zero as when angle is 0, sun is still visible.")
   "Use the calculation method defined by the Egyptian General Authority of Survey."
   (awqat--preset-with-angles -19.5 -17.5))
 
+(defun awqat-set-preset-kuwait ()
+  "Use the calculation method used in Kuwait."
+  (awqat--preset-with-angles -18.0 -17.5))
+
 (defun awqat-set-preset-jakim ()
   "Use the calculation method defined by the Department of Islamic Development Malaysia (JAKIM)."
   (awqat--preset-with-angles -20.0 -18.0))
