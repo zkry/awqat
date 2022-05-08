d6078ac7fd9bbdd6155a90b79eecf23b9a419344
Abdelhak Bougouffa <abougouffa@fedoraproject.org>
2022-05-08 03:36:44 +0200

    Add preset for Singapore

--- awqat.el
+++ awqat.el
@@ -137,6 +137,10 @@ This is not zero as when angle is 0, sun is still visible.")
   "Use the calculation method used in Kuwait."
   (awqat--preset-with-angles -18.0 -17.5))
 
+(defun awqat-set-preset-singapore ()
+  "Use the calculation method defined by the Majlis Ugama Islam Singapura."
+  (awqat--preset-with-angles -20.0 -18.0))
+
 (defun awqat-set-preset-jakim ()
   "Use the calculation method defined by the Department of Islamic Development Malaysia (JAKIM)."
   (awqat--preset-with-angles -20.0 -18.0))
